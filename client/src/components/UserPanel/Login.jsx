1249602958bc512af0090db6c79f3c4c98004f7d
Joshua Cruz <jcruz1998@gmail.com>
2024-09-10 18:56:01 -0400

    css

--- client/src/components/UserPanel/Login.jsx
+++ client/src/components/UserPanel/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import './auth.css';
 
 function Login({ setCurrentUser }) {
   const [username, setUsername] = useState('');
