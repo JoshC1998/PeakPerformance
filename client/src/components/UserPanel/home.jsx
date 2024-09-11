3917186fe930a81e99cdbab1af2dc194ec00592e
Joshua Cruz <jcruz1998@gmail.com>
2024-09-11 15:03:37 -0400

    css

--- client/src/components/UserPanel/home.jsx
+++ client/src/components/UserPanel/home.jsx
@@ -1,14 +1,17 @@
+// UserPanel/Home.jsx
 import React from 'react';
 import './home.css';
 
 function Home({ currentUser, handleLogout }) {
   return (
-    <div>
-      <h1>Welcome, {currentUser.username}!</h1>
-      <button className="logout-button" onClick={handleLogout}>
-        Logout
-      </button>
-      {/* Add other content or components you want on the home page */}
+    <div className="home-page">
+      <div className="content-container">
+        <h4>Welcome, {currentUser.username}!</h4>
+        <button className="logout-button" onClick={handleLogout}>
+          Logout
+        </button>
+        {/* Add other content or components you want on the home page */}
+      </div>
     </div>
   );
 }
